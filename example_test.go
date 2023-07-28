b1093eb47d6cd0774b3d1fe44dc352ed1769a22d
k1LoW <k1lowxb@gmail.com>
2023-07-28 14:31:43 +0900

    Add example

---
+++ example_test.go
@@ -0,0 +1,27 @@
+package driver_test
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func ExampleOpen() {
+	db, err := sql.Open("sqlite-vss", "test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	r := db.QueryRow("select vss_version();")
+	if err := r.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	var version string
+	if err := r.Scan(&version); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(version)
+}
